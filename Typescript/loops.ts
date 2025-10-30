fb9a2a094503770caf55bcbfc2c7de8ef73e571f
Praveen Roy <pkroy0505@gmail.com>
2025-10-30 22:31:27 +0530

    added loops

---
+++ Typescript/loops.ts
@@ -0,0 +1,120 @@
+// Loops - Which will execute the same block of code {...} multiple times
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+// Duplication
+// Maintenance
+// Remove time takeing process
+
+console.log("***********************");
+
+for(let i = 1 ; i<=5 ; i++){
+    console.log(i);
+}
+
+console.log("**********************************");
+
+// 1. for loop 
+        // a. for loop -  Traditional loop - if we know how many time we have to run the iteration
+        // b. for ...of loop - Loops over the iterable object - Array - [12,3,4,5]
+        // c. for ...in loop - Looping over the properties of an object - {key :value}
+
+// 2. while loop - If we do not know how many times we have to run the iteration
+// 3. do while loop - if we want to execute the loop atleast one time before chekcing the condition
+
+
+// 10th May 2026 // 7 //
+
+// {
+//     name : "Joe"
+//     age : 30
+// }
+
+
+
+// 1. for loop
+
+// Syntax:
+
+/*
+
+for(initialisation; condition; increment/decrement)
+{
+  // block of code
+  break
+}
+// initialisation - Initialise the value of a variable to start the execution. let i=1
+// condition - Condition to check whether the loop should continue or not. let i<=5
+// increment/decrement - Increase or decrease the value of the variable after each iteration. i++/i--
+
+*/
+
+// initialisation (let i = 10)
+// declaration (let i)
+
+for(let i =0; i<=5; ++i){ // 6<=5 - false
+    console.log(i);//  0 1 2 3 4 5          // 5++ = 5+1=6
+}
+
+console.log("************************");
+
+// 2. while loop
+
+// Syntax :
+
+/*
+ initialisation
+ while(condition)
+ {
+  // block of code
+  increment/decrement
+ }
+
+*/
+
+// 10 ... 1
+
+let a = 10
+while(a<=1){ // 0 >=1
+    console.log(a);
+    a--
+}
+
+console.log("*******************************");
+
+// 3. do while loop
+
+/*
+
+initialisation
+
+do 
+{
+  // block of code
+  increment/decrement  
+} while (condition);
+
+*/
+
+let j = 10
+
+do{
+    console.log(j);
+    j--
+} while(j<=1)
+
+
+
+
+
+
+
+// Function
+// Array
+// String - Methods
+// Object
+// Class
--- Typescript/statements.ts
+++ Typescript/statements.ts
@@ -9,4 +9,189 @@
 
 // Cross browser
 
+// 1. if statement - It will execute the if block of code only if condition is true
+
+
+// Syntax:
+/*
+if(condition){
+ // code
+}
+*/
+
+let isPopup = "popup"
+
+if(isPopup === "popup"){   // if("popup" == "Popup") // false
+    console.log("Close the popup");
+}
+
+// 2. if else condition - If the condition is true then if block of code of code will be execute otherwise the else block of code will be executed.
+
+// Syntax:
+
+/*
+if(condition){
+   // condition true - this block of code will be execute
+}
+else{
+   // else block of code 
+}
+*/
+
+// Admin, user
+
+const role = "normal"
+
+if(role === "Admin")
+{ // if("normal" === "Admin")
+    console.log("Admin user navigation");
+}
+else
+{
+    console.log("Normal user navigation");
+}
+
+
+// 3. if else if statement  - Will validate multiple conditions. depending on the result of the condition the specific block of code will be executed
+
+// Syntax:
+
+/*
+if(condition){
+  // if the condition is true this block of code
+}
+else if(condition1){
+ // if condition1 is true this block 
+}
+else if(condition2){
+ // if condition2 is true this block  
+}
+else{
+ // none of the condition is true
+}
+
+*/
+
+
+// Cross browser - 
+
+// Multiple environments - stage, qa, prod
+
+let browser = "Safari"
+
+if(browser === "chrome"){ // "Safari" === "chrome"
+    console.log("Launch the chrome browser");
+}
+else if(browser === "firefox"){ // "Safari" === "firefox"
+    console.log("Launch the firefox browser");
+}
+else if(browser == "safari"){ // "Safari" === "safari"
+    console.log("Launch the safari browser");
+}
+else{
+    console.log("Invalid broswer");
+}
+
+
+// 4. switch statement - 
+
+// Syntax:
+
+/*
+switch (expression){
+ case "value":
+    // block of code
+    break // it terminate the execution - loop or switch 
+ case "value1":
+    // block
+    break
+ default:
+    // block
+}
+*/
+
+let broswerName = "chrome"
+
+switch(broswerName){ // switch("chrome")
+    case "chrome":
+        console.log("launch the chrome browser in switch")
+        break
+    case "firefox":
+        console.log("launch the firefox browser in switch");
+        break
+    case "safari":
+        console.log("Launch the safari browser in switch");
+        break
+    default:
+        console.log("Invalid browser in switch");
+}
+
+
+// switch - switch will compare with the case value. It checks if any case value is avilable as similar to expression or not. If it is available it will directly jumpt to that block of code.
+// break keyword is mandatory in switch statement -  it terminate the execution - loop or switch
+
+// When to use if else if statement
+// 1. Range available in the condition to validate - we have to use if else if
+// 2. Multiple datatypes to be validate  - name = "Rahul", age >30, salary >200000
+// 3. Logical Operator for the validation - && , ||
+
+// When to use switch statement
+// 1. No range
+// 2. Single DT
+// 3. No Logical operator
+
+
+// 5. Ternary Operator (? :) - This is similar to if else but it use inline
+
+// Syntax:
+
+// condition? execute this if true : execute this if false
+
+let age = 10;
+
+const result = (age >= 18)? "yes" : "No"
+console.log(result);
+
+
+
+/*
+
+
+
+*/
+
+
+// >90 - A
+
+
+
+// == "Safari" == "safari"
+// === "Safari" == "safari"
+
+
+
+// Write a program to display the grade of a student
+// >90 - A
+// >80 - B
+// >70 - C
+// >50 - D
+// >=35 - E
+// <35 -Failed
+
+// Switch staetement to print the week name according to the day 1-7 - 
+// 1- Sunday
+// 2 - Monday ..... 7
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 
