ad8dcdc7cb3ac70193aa2e7a62d1242ebc7f1b3d
Praveen Roy <pkroy0505@gmail.com>
2025-11-07 22:30:19 +0530

    array method

--- Typescript/arrays.ts
+++ Typescript/arrays.ts
@@ -66,6 +66,7 @@ console.log(array4);
 
 //let array2 :any = ["TS", 12,23,true, null, 90] // 6 [0-5] - [0,1,2,3,4,5]
 
+// Property
 // length - Return the total number of element in an array
 console.log(array2.length);
 
@@ -97,9 +98,81 @@ for(let element of array2){
    }
 }
 
+console.log("************ Methods of an array******************");
+
+let array5 = ["Orange", "Apple", 10,20,30,40,50, true]
+console.log(array5);
+
+// 1. push(ele1, ele2, ele3)- Add the elements to the end of array
+// Syntax:
+// arrayName.push() 
+
+array5.push(100,200, "JS")
+console.log("Push", array5);
+
+// 2. pop() - Removes the last element from an array
+// Syntax:
+// arrayname.pop()
+console.log(array5.pop());
+console.log("pop", array5);
+
+// Difference between push() and unshift() method
+// Difference between pop() and shift() method
+
+// 3. unshift(parameters,...) - Adds the elements to the beginning of an array
+// Syntax:
+// arrayName.unshift(arguments,...)
+
+array5.unshift("TS",799)
+console.log("unshift()", array5);
+
+// 4. shift() - Removes the first element from an array
+// Syntax:
+// arrayName.shift()
+array5.shift()
+console.log(array5);
+
+let arr1 = [10,20,30,40,50,60, "TS", 50,60,50]
+
+// Difference between splice() and slice()
+
+// 5. splice(startIndex, deleteCount, ele1, ele2,....)
+// Syntax:
+// arrayName.splice(startIndex, deleteCount, ele1, ele2,....)
+
+// startIndex - The position where you want to add/delete the element
+// deleteCount - The number of element that you want to deletefrom an array at the startIndex
+// ...items - The element that you want to add at the startIndex
+
+arr1.splice(2, 1, 23,"pink",56)
+//let arr1 = [10,20,23,34,56,40,50,60]
+console.log("splice()", arr1);
+
+// 6. slice(startIndex?, endIndex?) - Return the portion of an array beteen the start and endIndex
+// Syntax:
+// arrayName.slice(startIndex, endIndex)
+
+// startIndex - The position where you want to start the slice
+// endIndex(Exclusive) [endIndex-1] - The position where you want to end the slice
+//  [ 10, 20, 23, 'pink', 56 , 40, 50, 60, 'TS']
+console.log(arr1.slice(3,7)); // 3,4,5
+console.log(arr1.slice(3));
+let arr2 = [10,20,30,40,50]
+console.log("Slice", arr2.slice().reverse());
+console.log(arr2);
 
 
+// 7. indexOf(element, startIndex?) - Return the index of first occurances of na element.
+// synatx:
+// arrayName.indexOf(element, startIndex)
+console.log(arr1);
+console.log(arr1.indexOf(500))
 
 
+// Take an array which have an element alteast repeated for 3-4 times. Write a program to find the number of that particular is available inside 
 
+// 1. [10,20,30,40,50,10,90,100,10,300,10,400,10] - to find number of times 10 appeared in this array
 
+// 2. 
+
+// 1. To find out the first duplicate element from an array
