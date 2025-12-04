e4cabf7db2e0207e9eae0b0f2f87779669d341e7
Praveen Roy <pkroy0505@gmail.com>
2025-12-04 22:27:10 +0530

    added radio and checkbox

--- tests/UIBasics.spec.ts
+++ tests/UIBasics.spec.ts
@@ -1,6 +1,7 @@
 import {test, expect} from '@playwright/test'
 
 
+
 test("Handling input field and clicking on an element", {tag: '@smoke'},async function({page}){
 
     // Launch the browser
@@ -21,7 +22,7 @@ test("Handling input field and clicking on an element", {tag: '@smoke'},async fu
 
     // toHaveValue("value") - This is an assertion method which helps us in validating if an "input" field have value as "value"
 
-    // toHaveText("text") - This is an assertion method which helps us in validating if an element have a text as "text"
+    // toHaveText("text") - This is an assertion method which helps us in validating if an element have exact the same text as "text"
 
     // toContainText("contain text") - This is an assertion method which helps us in validating if an element contains a text as "contain text"
 
@@ -39,4 +40,45 @@ test("Handling input field and clicking on an element", {tag: '@smoke'},async fu
     await expect(page.locator("h1.post-title")).toHaveText("Logged In Successfully", {timeout : 300000})
     await expect(page.locator("h1.post-title")).toContainText("Successfully")
 
+    // allTextContents()
+    // allInnerTexts()
+
+})
+
+
+test("Handling radio button and checkbox", async ({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/")
+    const radioBtn = page.getByRole('radio', {name : 'Female'})
+
+    // click() - Click on an element
+    // check() - It will first validate if the radio button or checkbox is already checked. If it is not already checked then it will click on the element.
+
+    // toBeChecked() - It help us in validating if the radio button or checkbox is selected/checked or not
+    await expect(radioBtn).not.toBeChecked()
+
+    await radioBtn.check({timeout: 60000})
+    await expect(radioBtn).toBeChecked()
+
+    const checkbox = page.getByRole('checkbox', {name: 'Thursday'})
+
+    await checkbox.check()
+    await checkbox.uncheck()
+
+    await expect(checkbox).not.toBeChecked()
+})
+
+
+test("Getting text from any element and from multiple elements", async ({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/")
+    // textContent() - Get the text value from an element even if the element is disabled
+    // innerText() - Get the text value ONLY from Visible element on the webpage
+
+    const singleText = await page.locator("h3.post-title").innerText()
+    console.log(singleText);
+
+    //allInnerTexts() / allTextContents()
+
+    const elements = page.locator("h2.title")
+    const multipleTexts = await elements.allInnerTexts()
+    console.log(multipleTexts);
 })
